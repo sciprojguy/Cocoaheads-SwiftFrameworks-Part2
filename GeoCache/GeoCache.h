b042cb429a6592ed83e43b198464b1bb89d9ac8e
Chris Woodard <sciprojguy@gmail.com>
2017-07-26 13:01:36 -0400

    Initial Commit

---
+++ GeoCache/GeoCache.h
@@ -0,0 +1,19 @@
+//
+//  GeoCache.h
+//  GeoCache
+//
+//  Created by Chris Woodard on 7/26/17.
+//  Copyright © 2017 UsefulSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoCache.
+FOUNDATION_EXPORT double GeoCacheVersionNumber;
+
+//! Project version string for GeoCache.
+FOUNDATION_EXPORT const unsigned char GeoCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoCache/PublicHeader.h>
+
+
